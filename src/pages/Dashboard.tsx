2bc3dffa08008503efca57c1eb86f6f2e46cf88f
Matt Abreu <mrmattabreu@gmail.com>
2021-01-17 15:45:52 +0000

    layout(DashboardPage): create page and add elements

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import PageHeader from "../components/PageHeader";
+
+function DashboardPage() {
+  return (
+    <div>
+      <PageHeader title="Dashboard" />
+      Welcome to Dashboard
+    </div>
+  );
+}
+
+export default DashboardPage;
