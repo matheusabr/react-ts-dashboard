8220297d40098ef23d7edd8b87c61d3eb13aa2fc
Matt Abreu <mrmattabreu@gmail.com>
2021-01-17 16:21:24 +0000

    design(PageHeader): apply style

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 
+import { COLORS } from "../styles/colors";
+
 interface Props {
   title: string;
 }
 
 const PageHeader: React.FC<Props> = ({ title }) => {
-  return <div>{title}</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        color: COLORS.grey.dark,
+        backgroundColor: COLORS.secondary,
+        fontWeight: "bold",
+        padding: 10,
+        height: 50,
+      }}
+    >
+      <span>{title}</span>
+    </div>
+  );
 };
 
 export default PageHeader;
