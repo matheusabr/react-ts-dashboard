bc69732e7f28d2c9306077b8f0609604120da7e3
Matt Abreu <mrmattabreu@gmail.com>
2021-01-17 15:46:07 +0000

    layout(SettingsPage): create page and add elements

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import PageHeader from "../components/PageHeader";
+
+function SettingsPage() {
+  return (
+    <div>
+      <PageHeader title="Settings" />
+      Settings details
+    </div>
+  );
+}
+
+export default SettingsPage;
