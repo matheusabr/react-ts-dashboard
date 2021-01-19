835a3a713c3e02fa1edfc29225a4b4e932a42cfe
Matt Abreu <mrmattabreu@gmail.com>
2021-01-19 20:08:46 +0000

    design(ListItem): handle optional props and layout

--- src/components/ui/ListItem.tsx
+++ src/components/ui/ListItem.tsx
@@ -8,10 +8,18 @@ interface Props {
 
 const ListItem: React.FC<Props> = ({ title, thumbnail, subtitle }) => {
   return (
-    <div style={{ display: "flex", height: 80, padding: "2px 0" }}>
-      <div style={{ height: 80, width: 80 }}>
-        <img style={{ height: 80, width: 80 }} src={thumbnail} alt={title} />
-      </div>
+    <div
+      style={{
+        display: "flex",
+        height: thumbnail ? 80 : 30,
+        padding: "2px 0",
+      }}
+    >
+      {thumbnail && (
+        <div style={{ height: 80, width: 80 }}>
+          <img style={{ height: 80, width: 80 }} src={thumbnail} alt={title} />
+        </div>
+      )}
       <div
         style={{
           flexGrow: 1,
@@ -23,12 +31,14 @@ const ListItem: React.FC<Props> = ({ title, thumbnail, subtitle }) => {
             display: "flex",
             flexDirection: "column",
             justifyContent: "space-evenly",
-            height: 80,
+            height: thumbnail ? 80 : "auto",
           }}
         >
-          <span style={{ fontSize: 14, fontWeight: "lighter" }}>
-            {subtitle}
-          </span>
+          {subtitle && (
+            <span style={{ fontSize: 14, fontWeight: "lighter" }}>
+              {subtitle}
+            </span>
+          )}
           <span style={{ fontSize: 16, fontWeight: "bold" }}>{title}</span>
         </div>
       </div>
