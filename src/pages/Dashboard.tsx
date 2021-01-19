41b4ab9af021b5d855c877aa5526e2a2afaa259e
Matt Abreu <mrmattabreu@gmail.com>
2021-01-19 18:48:48 +0000

    task(Dashboard): call get space news and show list

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import PageHeader from "../components/PageHeader";
+import ListItem from "../components/ui/ListItem";
+import { RootState } from "../store";
+import SpaceNewsActions from "../store/actions/spaceNewsActions";
 
 const DashboardPage: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const { spaceNews } = useSelector((state: RootState) => state);
+
+  useEffect(() => {
+    dispatch(SpaceNewsActions.getUserProfile({ limit: 3 }));
+  }, [dispatch]);
+
   return (
     <div>
       <PageHeader title="Dashboard" />
-      Welcome to Dashboard
+      <div style={{ padding: 10 }}>
+        <div style={{ paddingBottom: 20 }}>
+          <h3 style={{ margin: 0 }}>Space Flight News from API</h3>
+          <span>API: https://test.spaceflightnewsapi.net/api/v2/articles</span>
+        </div>
+        {spaceNews.news &&
+          spaceNews.news.map((item: any) => {
+            return (
+              <ListItem
+                key={item.id}
+                title={item.title}
+                thumbnail={item.imageUrl}
+                subtitle={item.publishedAt}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
