06365ac4dac6907441101cd9a3d023b5fb2995c2
Matt Abreu <mrmattabreu@gmail.com>
2021-01-19 03:37:03 +0000

    design(Button): apply style

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { COLORS } from "../../styles/colors";
 
 interface Props {
   label: string;
+  margin?: number | string;
   color?: string;
   backgroundColor?: string;
   disabled?: boolean;
@@ -10,14 +12,25 @@ interface Props {
 
 const Button: React.FC<Props> = ({
   label,
+  margin = 0,
   color = "#fcfcfc",
-  backgroundColor = "#3d3d3d",
+  backgroundColor = COLORS.grey.dark,
   disabled,
   onClick,
 }) => {
   return (
     <button
-      style={{ color, backgroundColor }}
+      style={{
+        height: 40,
+        border: 0,
+        borderRadius: 25,
+        padding: "5px 20px",
+        fontSize: 16,
+        fontWeight: "bold",
+        margin,
+        color,
+        backgroundColor,
+      }}
       disabled={disabled}
       onClick={onClick}
     >
