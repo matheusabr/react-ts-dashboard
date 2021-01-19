f03c0a7469714c96c6c274fa95b6979a3396098b
Matt Abreu <mrmattabreu@gmail.com>
2021-01-19 02:02:21 +0000

    task(Dashboard): update to typed function

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import PageHeader from "../components/PageHeader";
 
-function DashboardPage() {
+const DashboardPage: React.FC = () => {
   return (
     <div>
       <PageHeader title="Dashboard" />
       Welcome to Dashboard
     </div>
   );
-}
+};
 
 export default DashboardPage;
