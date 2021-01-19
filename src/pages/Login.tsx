c0557dfc4dfe9af196844a047ed5d0d545dbdb5c
Matt Abreu <mrmattabreu@gmail.com>
2021-01-19 03:40:42 +0000

    design(Login): apply style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,6 +5,7 @@ import AuthActions from "../store/actions/authActions";
 
 import TextField from "../components/ui/TextField";
 import Button from "../components/ui/Button";
+import { COLORS } from "../styles/colors";
 
 const LoginPage: React.FC = () => {
   const dispatch = useDispatch();
@@ -20,28 +21,52 @@ const LoginPage: React.FC = () => {
   }
 
   return (
-    <div>
-      <form
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: COLORS.grey.light,
+      }}
+    >
+      <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
+          backgroundColor: COLORS.secondary,
+          borderRadius: 20,
         }}
       >
-        <TextField
-          label="E-mail"
-          type="text"
-          handleChange={(event) => setEmail(event.target.value)}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          handleChange={(event) => setPassword(event.target.value)}
-        />
-        <Button label="Sign In" onClick={(event) => handleSignIn(event)} />
-      </form>
+        <form>
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              flexDirection: "column",
+              padding: 60,
+            }}
+          >
+            <TextField
+              label="E-mail"
+              type="text"
+              handleChange={(event) => setEmail(event.target.value)}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              handleChange={(event) => setPassword(event.target.value)}
+            />
+            <Button
+              label="Sign In"
+              margin={"10px 0 0 0"}
+              backgroundColor={COLORS.primary}
+              onClick={(event) => handleSignIn(event)}
+            />
+          </div>
+        </form>
+      </div>
     </div>
+    // </div>
   );
 };
 
